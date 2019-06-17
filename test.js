de73b4103c3f2687ab7106f6c27a0d998c0aa0af
Georgina Sarpong <georginasarpong.student@careerdevs.com>
2019-06-17 23:26:36 +0000

    commenting rules

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
+ //first alert
  alert("Hello World");
+ 
+ // second alert
+ alert("Hello, World!");
+ 
+ 
+ // make the image invisible
+ // move the image from top to bottom
+ // make image visible
