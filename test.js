c5945752cb325763eedd4070e9aaf602af6951d6
Georgina Sarpong <georginasarpong.student@careerdevs.com>
2019-06-18 00:43:56 +0000

    Practiced loops pt2 with breaks

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
-var links = document.getElementsByTagName("a");
-
-for(var i = 1; i <= links.length; i++) {
-    console.log("this is link number" + i);
+for ( var i = 0; i < 10; i++) {
+    if ( i === 5 || i === 3){
+        continue;
+    }
+    console.log(i);
+    if (i === 7) {
+        break;
+    }
 }
-
-document.write("all links looped");
- 
+console.log( "I have broken out of the loop");
