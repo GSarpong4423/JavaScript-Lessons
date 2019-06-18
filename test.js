e30e30179b36ebc821ae155bb0e17b091782499c
Georgina Sarpong <georginasarpong.student@careerdevs.com>
2019-06-18 00:02:28 +0000

    else if statements

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
- var youLikeMeat = true;
- var myNum = 10;
- 
- if (youLikeMeat) {
-     document.write("you like meat");
-     
- } else {
-     document.write("you hate meat");
- }
+var myAge = 29;
+
+if (myAge > 30) {
+    document.write("yo are over 30!");
+} else if (myAge > 20) {
+    document.write("you are over 20!");
+} else if (myAge > 10) {
+    document.write("you are over 10!");
+} else {
+    document.write ("you are not over 10!");
+}
  
